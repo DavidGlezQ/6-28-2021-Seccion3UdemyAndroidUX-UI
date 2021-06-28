3c781174c0ad5855e83fbe9139f83a8b4717be71
DavidGlezQ <62410785+DavidGlezQ@users.noreply.github.com>
2021-06-28 18:15:15 -0500

    6-28-2021-Initial-Commit
    Creación del proyecto.

---
+++ app/src/main/java/com/davidglez/a6_28_2021_seccion3udemyandroidux_ui/MainActivity.java
@@ -0,0 +1,14 @@
+package com.davidglez.a6_28_2021_seccion3udemyandroidux_ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
