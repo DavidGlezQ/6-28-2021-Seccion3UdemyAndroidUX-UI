54c5b564ec50a50d7e7be4daafa90539ebb87ae7
DavidGlezQ <62410785+DavidGlezQ@users.noreply.github.com>
2021-06-29 16:46:37 -0500

    6-29-2021-Refact-Clase18

--- app/src/main/java/com/davidglez/a6_28_2021_seccion3udemyandroidux_ui/utils/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.davidglez.a6_28_2021_seccion3udemyandroidux_ui.utils;
-
-public class Constants {
-    public static final String ARG_NAME = "name";
-}
--- app/src/main/java/com/davidglez/a6_28_2021_seccion3udemyandroidux_ui/utils/OnClickListener.java
+++
@@ -1,5 +0,0 @@
-package com.davidglez.a6_28_2021_seccion3udemyandroidux_ui.utils;
-
-public interface OnClickListener {
-    void onClick(PojoComponent pojoComponent);
-}
